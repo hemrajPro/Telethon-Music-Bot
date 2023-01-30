4f5f03af48c1c8d3ace2593c488dff8ce7563786
Hemraj <hemrajdangi2210@gmail.com>
2023-01-30 18:47:46 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -10,7 +10,7 @@ class Config(object):
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
     SUPPORT = os.environ.get("SUPPORT", "DANGI_BOTS_Support") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "DANGI_BOTS") # Your Channel
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/DANGI-01-30")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
     ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "")) # telegram I'd not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
